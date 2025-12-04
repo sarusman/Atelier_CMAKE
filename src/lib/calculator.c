69ef2a850e53bd3a8f0e93013a7d6afeb85029f7
Saru <sarusman@gmail.com>
2025-12-04 09:41:06 +0100

    Séquence 3 : Exercice 1

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -15,4 +15,6 @@ double _mul(double a, double b) {
 double _div(double a, double b) {
 	return a/b;
 }
-	
+double _car(double a) {
+    return a * a;
+}
--- src/main.c
+++ src/main.c
@@ -4,32 +4,54 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+    if (argc < 3 || argc > 4) {
+        printf("Erreur de parametres");
+        return 1;
+    }
 
-		
+    char* op = argv[1];
+    double r = 0;
+
+    // cas des opérations à 2 opérandes
+    if (strcmp(op, "add") == 0 ||
+        strcmp(op, "sub") == 0 ||
+        strcmp(op, "mul") == 0 ||
+        strcmp(op, "div") == 0) {
+
+        if (argc != 4) {
+            printf("Erreur de parametres");
+            return 1;
+        }
+
+        char* a = argv[2];
+        char* b = argv[3];
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+        } else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+        } else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+        } else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+        }
+
+        printf("%lf", r);
+    }
+    else if (strcmp(op, "car") == 0) {
+        if (argc != 3) {
+            printf("Erreur de parametres");
+            return 1;
+        }
+
+        char* a = argv[2];
+        r = _car(atof(a));
+        printf("%lf", r);
+    }
+    else {
+        printf("Erreur de parametres");
+        return 1;
+    }
+
+    return 0;
+}
